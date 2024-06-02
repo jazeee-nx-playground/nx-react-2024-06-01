deda233a6facfaa355d3a6f5ffe920bc85aa7f03
Jaz Singh <jazeee@gmail.com>
2024-06-01 17:52:58 -0700

    Adjust app

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,16 +1,11 @@
-import styled from '@emotion/styled';
-
 import NxWelcome from './nx-welcome';
-
-const StyledApp = styled.div`
-  // Your style here
-`;
 
 export function App() {
   return (
-    <StyledApp>
+    <div>
+      test
       <NxWelcome title="nx-react-2024-06-01" />
-    </StyledApp>
+    </div>
   );
 }
 
