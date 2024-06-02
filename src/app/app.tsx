574a95840f38ce8db9c607c716dd0d28105145d5
Jaz Singh <jazeee@gmail.com>
2024-06-01 17:40:23 -0700

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,17 @@
+import styled from '@emotion/styled';
+
+import NxWelcome from './nx-welcome';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      <NxWelcome title="nx-react-2024-06-01" />
+    </StyledApp>
+  );
+}
+
+export default App;
