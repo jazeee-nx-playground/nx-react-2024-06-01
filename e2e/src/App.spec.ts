3369307a9bd7873732c65f1aee1e9b9f5ba6ff58
Jaz Singh <jazeee@gmail.com>
2024-06-02 18:55:03 -0700

    Test the child page
    Summary
    ===
    The E2E test didn't have enough coverage.
    
    Changes
    ===
    - Update test to click on link and verify child page content.

--- e2e/src/App.spec.ts
+++ e2e/src/App.spec.ts
@@ -3,7 +3,9 @@ import { test, expect } from '@playwright/test';
 test('App', async ({ page }) => {
   await page.goto('/');
 
-  await expect(
-    page.getByRole('link', { name: 'Go to Test Page' })
-  ).toBeInViewport();
+  const testPageLink = page.getByRole('link', { name: 'Go to Test Page' });
+  await expect(testPageLink).toBeVisible();
+  await testPageLink.click();
+  await expect(page.getByText('This is a test')).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Hello World' })).toBeVisible();
 });
